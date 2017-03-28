36ce477261b93a56ef65ec290d7958ed34e28ba4
Charlie Callow <wollac11@gmail.com>
2017-03-28 15:52:57 +0100

    Add provider for Mendeley Desktop

---
+++ apps/mendeley.sh
@@ -0,0 +1,13 @@
+munki_name="Mendeley Desktop"
+munki_path="mendeley.com"
+down_url="http://www.mendeley.com/autoupdates/installer/Mac-x64/stable-incoming"
+
+# Checks latest version of app available online
+check_avail_Mendeley_Desktop() {
+        # Determine latest version available to download and store version
+        # string for comparison to Munki repo version
+        avversion=$(wget --spider -S --max-redirect 1 "http://www.mendeley.com/autoupdates/installer/Mac-x64/stable-incoming" 2>&1 |  sed -n 's/^.*Mendeley-Desktop-\([^&]*\)-OSX-Universal.dmg/\1/p;' | head -1)
+        echo "${avversion}" # output discovered version
+}
+
+
