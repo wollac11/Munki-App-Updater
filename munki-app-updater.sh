f03cf23e9bf0fc7079b9505f82794a3e5647a20b
Charlie Callow <wollac11@gmail.com>
2017-03-30 11:28:07 +0100

    Fix error in download path var check

--- munki-app-updater.sh
+++ munki-app-updater.sh
@@ -139,7 +139,7 @@ update_app() {
 	# Clear previous downloads
 	echo "Deleting old downloads..."
   # Check download_path set (for saftey!!)
-  if [ -v $download_path ] then
+  if [ -v $download_path ]; then
         # Clear contents of download path
         rm -rf "${download_path}"/*
   fi
